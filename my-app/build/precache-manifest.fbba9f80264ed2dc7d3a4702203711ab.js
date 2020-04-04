e5ce2af257b395345da76c4bbddac934877f9234
aozycz <alexozycz@icloud.com>
2020-04-04 05:56:19 +0000

    build folder

---
+++ my-app/build/precache-manifest.fbba9f80264ed2dc7d3a4702203711ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e04aea05379c5a8831488843bf640a8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aaa7cd5a5266869f77c5",
+    "url": "/static/css/main.aff23b6a.chunk.css"
+  },
+  {
+    "revision": "cc5b60f530ec9ef2959d",
+    "url": "/static/js/2.3dd64d21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3dd64d21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaa7cd5a5266869f77c5",
+    "url": "/static/js/main.4b5e4d77.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
